333c82ba60143a9dd7480a5e8c1c966716e63caa
Eberth <the.jonkey@gmail.com>
2022-04-30 15:19:56 -0500

    Actualizacion del crawler a uploader

---
+++ solr-indexer-uploader/app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from router import routes
+
+import uvicorn
+
+
+app = FastAPI(docs_url="/")
+
+app.include_router(routes.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host='localhost',
+                port=8094, reload=True, debug=True)
