ad0bffba0738c04a22d9cfd5c9a3ee7808e12316
ArturoAguilar <arturo.aguilar@transpara.com>
2022-04-30 23:52:34 -0500

    add upload to html

--- solr-indexer-uploader/app/main.py
+++ solr-indexer-uploader/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from router import routes
-
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 
 app = FastAPI(docs_url="/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routes.router)
 
